04a5ae6baf11d67f72f2418ad2a3c4452ea36fb0
Kalvin Zhao <kalvinzhao@kalvins-mbp.home>
2020-08-14 11:37:55 -0400

    added characters in

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios'
 import './App.css';
+import Character from './components/Character'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,10 +10,22 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
+  const [characters, setCharacters] = useState()
+  useEffect(()=>{
+    axios.get('https://swapi.dev/api/people')
+      .then(response =>{
+        // console.log(response)
+        setCharacters(response.data)
+      })
+      .catch(error =>{
+        debugger
+      })
+  },[])
+  if (!characters) return <div><h1>Loading ...</h1></div>
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character characters={characters}/>
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,25 @@
 // Write your Character component here
+import React, {useState} from 'react'
+
+const Character = (props) => {
+    const characters = props
+    console.log(characters.characters.results)
+    return (
+        <div>
+            {
+                characters.characters.results.map(character =>{
+                    return ( 
+                        <div>
+                            <h2>{character.name}</h2>
+                            <p>{character.gender}</p>
+                            <p>{character.birth_year}</p>
+                        </div>
+                            
+                    );
+                })
+            }
+        </div>
+    )
+}
+
+export default Character
